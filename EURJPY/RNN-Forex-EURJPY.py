c8ff87e7d3e2a728477e4b47bf1fd22d01e6aaec
alifiasyalsa <alifiasyalsabila96@gmail.com>
2019-05-16 11:31:23 +0700

    Data EURJPY & RNN di folder EURJPY

---
+++ EURJPY/RNN-Forex-EURJPY.py
@@ -0,0 +1,103 @@
+# Recurrent Neural Network
+
+# Part 1 - Data Preprocessing
+
+# Importing the libraries
+import numpy as np  # numpy is a math library in python that enables us to do computattion
+import matplotlib.pyplot as plt # library for data vizualization 
+import pandas as pd # library for data analysis
+
+# Importing the training set
+dataset_train_ = pd.read_csv('D:\.Semester 4\Proyek II\HERE\ForexPrediction\EURJPY\EURJPY_new_train.csv')
+training_set_ = dataset_train_.iloc[:, 2:3].values
+
+# Feature Scaling
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range = (0, 1))
+training_set_scaled = sc.fit_transform(training_set_)
+
+# Creating a data structure with 60 timesteps and 1 output
+X_train = []
+y_train = []
+for i in range(60, len(training_set_scaled)): #terdapat 1258 record dengan start dari 61 dimana 60 data sebelumnya disimpan di X_train dan data ke 61 hingga selanjutnya di Y_Train
+    X_train.append(training_set_scaled[i-60:i, 0])  
+    y_train.append(training_set_scaled[i, 0])
+X_train, y_train = np.array(X_train), np.array(y_train)
+
+# Reshaping
+X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+
+
+# Part 2 - Building the RNN
+
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
+
+# Initialising the RNN
+regressor = Sequential()
+
+# Adding the first LSTM layer and some Dropout regularisation
+
+regressor.add(LSTM(units = 70, return_sequences = True, input_shape = (X_train.shape[1], 1)))
+regressor.add(Dropout(0.2))
+
+# Adding a second LSTM layer and some Dropout regularisation
+regressor.add(LSTM(units = 70, return_sequences = True))
+regressor.add(Dropout(0.2))
+
+# Adding a third LSTM layer and some Dropout regularisation
+regressor.add(LSTM(units = 70, return_sequences = True))
+regressor.add(Dropout(0.2))
+
+# Adding a forth LSTM layer and some Dropout regularisation
+regressor.add(LSTM(units = 70, return_sequences = True))
+regressor.add(Dropout(0.2))
+
+# Adding a fifth LSTM layer and some Dropout regularisation
+regressor.add(LSTM(units = 70, return_sequences = True))
+regressor.add(Dropout(0.2))
+
+# Adding a fourth LSTM layer and some Dropout regularisation
+regressor.add(LSTM(units = 70))
+regressor.add(Dropout(0.2))
+
+# Adding the output layer
+regressor.add(Dense(units = 1))
+
+# Compiling the RNN
+regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
+
+# Fitting the RNN to the Training set
+regressor.fit(X_train, y_train, epochs = 100, batch_size = 32)
+
+
+# Part 3 - Making the predictions and visualising the results
+
+# Getting the real stock price of 2017
+dataset_test = pd.read_csv('D:\.Semester 4\Proyek II\HERE\ForexPrediction\EURJPY\EURJPY_new_test.csv')
+real_stock_price = dataset_test.iloc[:, 2:3].values
+
+# Getting the predicted stock price of 2017
+dataset_total = pd.concat((dataset_train_['<OPEN>'], dataset_test['<OPEN>']), axis = 0)
+inputs = dataset_total[len(dataset_total) - len(dataset_test) - 60:].values
+inputs = inputs.reshape(-1,1)
+inputs = sc.transform(inputs)
+X_test = []
+for i in range(60, 120):
+    X_test.append(inputs[i-60:i, 0])
+X_test = np.array(X_test)
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+predicted_stock_price = regressor.predict(X_test)
+predicted_stock_price = sc.inverse_transform(predicted_stock_price)
+
+# Visualising the results
+plt.plot(real_stock_price, color = 'red', label = 'Real Forex Price')
+plt.plot(predicted_stock_price, color = 'blue', label = 'Predicted Forex Price')
+plt.title('Forex Open Price Prediction')
+plt.xlabel('Time')
+plt.ylabel('Forex Price')
+plt.legend()
+plt.show()
